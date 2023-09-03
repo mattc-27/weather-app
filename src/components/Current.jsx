673c7956fe085bbbd7324db7f878a63f4af16fa8
mattc-27 <mattcopeland927@gmail.com>
2023-09-02 19:02:40 -0600

    updated styling and layout

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,126 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { WiHumidity, WiCloudUp, WiCloudDown } from "react-icons/wi";
 import { GiWindTurbine } from "react-icons/gi";
-import { BsSunrise, BsSunset } from "react-icons/bs";
-import { AiOutlineCloseCircle } from 'react-icons/ai';
-import HourlyForecast from './HourlyForecast';
-import { ConditionsCard } from './ConditionsCard';
 import '../style.css';
 
-
-function Current({ weather }) {
-
-    const [showComponent, setShowComponent] = useState(false);
-    useEffect(() => {
-        const delay = 2000; // 2 seconds
-        const timeoutId = setTimeout(() => {
-            setShowComponent(true);
-        }, delay);
-        return () => clearTimeout(timeoutId); // Clean up the timeout when the component unmounts
-    }, []);
-
-    /// Current conditions
-    function Conditions() {
-        return (
-            <>
-                <div className="conditionsCenter">
-                    <div className="currentIcon">
-                        <p>{weather.formatIcon.value}</p>
-                    </div>
-                    <div className="currentTemp">
-                        <p>{weather.temp_f.toFixed()}°F</p>
-                    </div>
-                </div>
-                <div className="currentDesc">
-                    <p>{weather.condition.text}</p>
-                </div>
-            </>
-        );
-    }
+/// Current conditions
+export default function Conditions({ weather }) {
 
     /// Day overview
-    function DayForecast() {
+    function DayForecast({ weather }) {
         return (
-            <div className="dayConditions">
-                <ConditionsCard
-                    icon1={<WiCloudUp size={35} />}
-                    condition1={`${weather.forecastday[0].day.maxtemp_f.toFixed()}°F`}
-                    icon2={<WiCloudDown size={35} />}
-                    condition2={`${weather.forecastday[0].day.mintemp_f.toFixed()}°F`}
-                />
-                <ConditionsCard
-                    icon1={<GiWindTurbine size={35} />}
-                    condition1={`${weather.wind_mph}mph`}
-                    icon2={<WiHumidity size={35} />}
-                    condition2={`${weather.humidity}%`}
-                />
-                <ConditionsCard
-                    icon1={<BsSunrise size={35} />}
-                    condition1={weather.forecastday[0].astro.sunrise}
-                    icon2={<BsSunset size={35} />}
-                    condition2={weather.forecastday[0].astro.sunset}
-                />
+            <div className='conditions-card'>
+                <div className='card-item'>
+                    <p><GiWindTurbine size={45} color={'#fff'} /></p>
+                    <p >{weather.wind_mph}</p>
+                </div>
+                <div className='card-item'>
+                    <p><WiCloudUp size={45} color={'#fff'} /></p>
+                    <p >{weather.forecastday[0].day.maxtemp_f.toFixed()}°F</p>
+                </div>
+                <div className='card-item'>
+                    <p><WiCloudDown size={45} color={'#fff'} /></p>
+                    <p >{weather.forecastday[0].day.mintemp_f.toFixed()}°F</p>
+                </div>
+                <div className='card-item'>
+                    <p><WiHumidity size={45} color={'#fff'} /></p>
+                    <p >{weather.humidity}%</p>
+                </div>
             </div>
         );
     }
 
     return (
         <>
-            {showComponent &&
-                <>
-                    <div className='conditions'>
-                        <div className='conditionsMain' >
-                            <div className='conditionsTop'>
-                                <div className="currentLocation">
-                                    <p>{weather.name}, {weather.region}</p>
-                                </div>
-                                <Conditions weather={weather} />
-                            </div>
-                            <div className='conditionsBottom'>
-                                <DayForecast weather={weather} />
-                            </div>
-                            <div className='outlookContainer'>
-                                <HourlyForecast weather={weather} />
-                            </div>
+            <div className='col content-even container-style ' style={{ flexBasis: '500px' }}>
+                <div className='row content-center items-center w-100 text-lg text-center'>
+                    <p>{weather.name}, {weather.region}</p>
+                </div>
+                <div className='col content-center items-center'>
+                    <div className='row content-center items-center text-sm mbt-2'>
+                        <div className="text-center">
+                            <p>{weather.formatIcon.value}</p>
                         </div>
+                        <div className='text-center text-md'>
+                            <p>{weather.temp_f.toFixed()}°F</p>
+                        </div>
+
                     </div>
-                </>
-            }
+                    <div className='text-center text-xmd mbt-2'>
+                        <p>{weather.condition.text}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col content-center items-center container-style mbt-2' style={{ flexBasis: '200px' }}>
+                <DayForecast weather={weather} />
+            </div>
         </>
     );
 }
-
-export default Current;
-
-
-/*   <div className="tempHighLow">
-                    <div className="tempHigh">
-                        <p><WiCloudUp size={35} /></p>
-                        <p>{weather.forecastday[0].day.maxtemp_f.toFixed()}°F</p>
-                    </div>
-                    <div className="tempLow">
-                        <p><WiCloudDown size={35} /></p>
-                        <p>{weather.forecastday[0].day.mintemp_f.toFixed()}°F</p>
-                    </div>
-                </div>
-                <div className="tempHighLow">
-                    <div className="tempHigh">
-                        <p><GiWindTurbine size={35} /></p>
-                        <p>{weather.wind_mph}mph</p>
-                    </div>
-                    <div className="tempLow">
-                        <p><WiHumidity size={35} /></p>
-                        <p>{weather.humidity}%</p>
-                    </div>
-                </div>
-                <div className="tempHighLow">
-                    <div className="tempHigh">
-                        <p><BsSunrise size={35} /></p>
-                        <p>{weather.forecastday[0].astro.sunrise}</p>
-                    </div>
-                    <div className="tempLow">
-                        <p><BsSunset size={35} /></p>
-                        <p>{weather.forecastday[0].astro.sunset}</p>
-                    </div>
-                </div>
-                */
